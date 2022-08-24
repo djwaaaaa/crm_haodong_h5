363299ac3eed122f7ec4b1551bdc7d3b8f5b5b44
9527 <2393863000@qq.com>
2022-08-24 17:02:28 +0800

    '2022-8-24-17:02_modify_bug'

--- src/api/service.js
+++ src/api/service.js
@@ -33,11 +33,11 @@ function createService () {
       } else {
         // 有 code 代表这是一个后端接口 可以进行进一步的判断
         switch (code) {
-          case 0:
+          case 1:
             // [ 示例 ] code === 0 代表没有错误
             // TODO 可能结果还需要code和msg进行后续处理，所以去掉.data返回全部结果
             // return dataAxios.data
-            return dataAxios
+            return dataAxios  
           case 'xxx':
             // [ 示例 ] 其它和后台约定的 code
             errorCreate(`[ code: xxx ] ${dataAxios.msg}: ${response.config.url}`)
